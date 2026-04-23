c093b8ab2c38d4b2524015a176ced2807eef92ac
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-04-23 18:14:37 -0500

    Agregar ejercicios for - centenas, mensajes y tabla del 3

---
+++ repaso.js
@@ -0,0 +1,68 @@
+function mostrar(opcion) {
+    switch (opcion) {
+        case 1:
+            ejercicio1();
+            break;
+        case 2:
+            imprimirCentenas();
+            break;
+        case 3:
+            imprimirCentenasRegresivo();
+            break;
+        case 4:
+            mostrarMensaje1();
+            break;
+        case 5:
+            mostrarMensaje2();
+            break;
+        case 6:
+            mostrarMensaje3();
+            break;
+        case 7:
+            mostrarMensaje4();
+            break;
+        case 8:
+            imprimirTabla3();
+            break;
+    }
+}
+function ejercicio1() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+function imprimirCentenas() {
+    for (let i = 100; i <= 1000; i += 100) {
+        console.log(i);
+    }
+}
+function imprimirCentenasRegresivo() {
+    for (let i = 1000; i >= 800; i -= 100) {
+        console.log(i);
+    }
+}
+function mostrarMensaje1() {
+    for (let i = 5; i < 8; i++) {
+        console.log("MENSAJE 1");
+    }
+}
+function mostrarMensaje2() {
+    for (let i = 10; i >= 8; i--) {
+        console.log("MENSAJE 2");
+    }
+}
+function mostrarMensaje3() {
+    for (let i = 0; i < 3; i++) {
+        console.log("MENSAJE 3");
+    }
+}
+function mostrarMensaje4() {
+    for (let i = 8; i > 5; i--) {
+        console.log("MENSAJE 4");
+    }
+}
+function imprimirTabla3() {
+    for (let i = 1; i <= 10; i++) {
+        console.log("3x" + i + "=" + (3 * i));
+    }
+}
